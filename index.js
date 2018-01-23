0e39f25b8a4fa7a13dc37fad25649988b515c9a4
Ashley Ryan <ashleyskyeryan@gmail.com>
2018-01-23 02:16:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code solution here
+class President {
+  constructor(name, politicalParty, yearsInOffice, homeState) {
+  this.name = name;
+  this.politicalParty = politicalParty;
+  this.yearsInOffice = yearsInOffice;
+  this.homeState = homeState;
+}
+  veto() {
+    console.log("NO!");
+  }
+
+  passBill() {
+    console.log("You can do that!");
+  }
+
+  doCharity() {
+    console.log("I like to help people.")
+  }
+
+  
+  
+}
